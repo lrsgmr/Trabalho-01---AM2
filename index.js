4d7fe1697f02ffa55b15385839b2c98a9567a579
lrsgmr <coldbreeze23@yahoo.com>
2021-08-28 11:16:24 -0300

    Atualizações

--- index.js
+++ index.js
@@ -1,4 +1,14 @@
-
+/*
+* Exemplo de aplicação Node.js com MongoDB
+* Autor: Wellington Wagner F. Sarmento
+* 
+*
+* Este exemplo foi baseado no código escrito em:
+* Utilizando a engine EJS para aplicações em NodeJS 
+* (https://www.codementor.io/@nulldreams/utilizando-a-engine-ejs-para-aplicacoes-em-nodejs-dok81l3si)
+*
+*
+*/
 
 const express = require("express");
 const app = express();
@@ -34,7 +44,12 @@ for (let cont=0;cont<20;cont++){
 app.set('view engine','ejs');
 app.use(expressLayouts);
 
-app.use(express.urlencoded({extended:false})); 
+app.use(express.urlencoded({extended:false})); //prepara a aplicacao para receber dados na forma de query string
+app.use(express.json()); //prepara a aplicacao para receber dados no formato JSON
+
+//esse codigo abaixo foi criado para testar variacoes de tempo de resposta do servidor, simulando problemas de lentidao na conexcao e/ou retardos no tempo de resposta do computador onde esta a aplicacao (servidor)
+//espera um tempo de 2 segundos (2000 milissegundos) para responder a qualquer requisicao do cliente. Isso gera um delay (espera) proposital para testes do codigo e condicoes de erro.
+//app.use(utils.delay(2000));
 
 
 
