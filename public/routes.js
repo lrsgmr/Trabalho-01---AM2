ddbbf0493324201cae3b90c8761527ad3422ab78
lrsgmr <coldbreeze23@yahoo.com>
2021-08-31 17:58:15 -0300

    Att AM2

--- public/routes.js
+++ public/routes.js
@@ -2,43 +2,26 @@ const express = require("express");
 const router = express.Router();
 
 
-//Especifica a pasta contendo arquivos estáticos. 
-//O nome 'public' não precisará ser colocado na rota 
-//Para serem alcançados os arquivos e pastas que estão dentro dele. 
-//Por isso na imagem que está na página home.ejs só há o indicativo para 'images'
-router.use(express.static('public'));
-
-//************* Exemplode Rotas ************* 
-
-/*
-http://localhost:3030/css
-http://localhost:3030/images
-http://localhost:3030/index.html
-/ = http://localhost:3030/
-/about = http://localhost:3030/about
-/cadastro =  http://localhost:3030/cadastro
-*/
 
 
-//Req é um objeto que recebe dados da requisição HTTP feita (request). Res permite enviar uma resposta ao navegador (Response)
-router.get('/',(req,res)=>{ //callback - funcao que trata dado evento GET
+
+router.get('/',(req,res)=>{ 
     res.render('pages/home');
 });
 
-router.get('/about',(req,res)=>{ //callback - funcao que trata dado evento  GET
+router.get('/about',(req,res)=>{ 
 
     res.render('pages/about');
 });
 
-router.get('/cadastro',(req,res)=>{ //callback - funcao que trata dado evento  GET
+router.get('/cadastro',(req,res)=>{ 
 
-    //a funcao render pode receber um pametro na forma de objeto literal
-    //no caso, ela irá receber um objeto com campo chamado users e com valor igual ao vetor users
+    
     res.render('pages/cadastro',{users:users}); 
 });
 
 router.post('/cadastro/remove',(req,res)=>{
-    //let item =req.body.id; //pega o valor passado através do parâmetro id e atribui a variável item. 
+    
     let name = req.body.name;
 
     if(users.length==0){
@@ -57,7 +40,7 @@ router.post('/cadastro/remove',(req,res)=>{
                     status:'sucess',
                     data:users
                 });
-                //res.send(JSON.stringify({sucess:`Elemento removido com sucesso: ${name}`}));
+                
             } else if(cont==users.length-1){
                 console.log("Erro ao remover elemento: ",name);
                 return res.status(400).json({
@@ -69,20 +52,15 @@ router.post('/cadastro/remove',(req,res)=>{
     }
     
     
-    //users.splice(item,1); //este método permite adicionar ou remover um item do vetor em uma dada posição. 
-    //res.render('pages/cadastro',{users:users});
-    //res.sendStatus(200); //envia mensagem 200 significando que as modificacoes foram ok
-    //res.send(JSON.stringify({sucess:`Elemento removido com sucesso: ${name}`}));
-    //console.log("Elemento Removido: ",name);
+    
     
 });
 
 
 router.post('/cadastro/update',(req,res)=>{
-    //substitui os valores armazenados no item do vetror dado por id, por valores fornecidos como parametro vindos do navegador.
-    //recebe dados do cliente na forma de um objeto JSON
+   
 
-    users[req.body.id].name=req.body.name; //ID do objeto ou Tag: name
+    users[req.body.id].name=req.body.name; 
     users[req.body.id].email=req.body.email;
     users[req.body.id].address=req.body.address;
     users[req.body.id].age=req.body.age;
@@ -90,12 +68,11 @@ router.post('/cadastro/update',(req,res)=>{
     users[req.body.id].vote=req.body.vote;
 
 
-    res.sendStatus(200); //envia mensagem 200 significando que as modificacoes foram ok
-    console.log("Dados recebidos: ",req.body);//mostra no console do servidor os dados recebidos
-});
+    res.sendStatus(200); 
+    console.log("Dados recebidos: ",req.body);
 
 router.get('/cadastro/list',(req,res)=>{
-    //Para fazer em casa: Como seria uma rotina para listar todos os itens cadastrados?
+    
 
 });
 
@@ -111,14 +88,13 @@ router.post('/cadastro/add',(req,res)=>{
 
     users.push(user);
     console.log("Usuário cadastrado: ",user);
-    console.log("Lista dos usuários: ",users); //nao use esta linha se tiver muitos elementos em users pois causara lentidao no servidor
+    console.log("Lista dos usuários: ",users); 
     res.sendStatus(200);
     res.status(200).json({
         status:'sucess',
-        data: `Usuário ${user} foi adiocionado com sucesso!`
+        data: `Usuário ${user} foi adicionado com sucesso!`
     });
 
 });
 
-//Essa linha permite que este código seja exportado como um módulo e possa ser usado em outras partes da aplicação.
 module.exports = router;
--- public/script.js
+++ public/script.js
@@ -2,26 +2,25 @@ let cadastro;
 
 
 function update(index,link){
-    //seleciona todas as tags que sejam td 
+    
     let tds = document.querySelectorAll(`td[data-index-row='${index}']`);
     let spans = document.querySelectorAll(`td[data-index-row='${index}'] > span`);
     let inputs = document.querySelectorAll(`td[data-index-row='${index}'] > input`);
 
-    let lenTds = tds.length-1; //numero de tds de uma linha da tabela
-    let linkUpdate = tds[lenTds-1]; //retorna o conteudo da penultima td, no caso, o link de update
+    let lenTds = tds.length-1; 
+    let linkUpdate = tds[lenTds-1]; 
     let linkRemove = tds[lenTds];
 
-    let lenInputs = inputs.length; //pega numero de inputs
+    let lenInputs = inputs.length; 
 
-    let button = inputs[lenInputs-1]; //cria uma conexao com o input que é do tipo button
-
+    let button = inputs[lenInputs-1]; 
 
 
     linkUpdate.className='hidden';
     linkRemove.className='hidden';
-    tds[lenTds-2].className='show'; //mostra butao de envio
+    tds[lenTds-2].className='show'; 
 
-     //esconde todos os campos de exibição de dados do cadastro
+     
     for(let cont=0;cont<spans.length;cont++){
         if(spans[cont].className=="show"){
             spans[cont].className="hidden";
@@ -29,52 +28,36 @@ function update(index,link){
             spans[cont].className="show";
         }
     }
-    //mostra os campos de preenchimento para o cadastro
+   
     for(let cont=0;cont<inputs.length;cont++){
         if(inputs[cont].className=="hidden"){
             inputs[cont].className="show";
         }
     }
 
-    //escuta se o botao foi clicado
+   
     button.addEventListener('click',()=>{
-        const http = new XMLHttpRequest(); //XHR - cria um objeto para requisição ao servidor
-        const url=link; //"/cadastro/update";
+        const http = new XMLHttpRequest(); 
+        const url=link; 
         let data = {id:"",name:"",email:"",address:"",age:"",heigth:"",vote:""};
         let dataToSend;
 
 
 
-        http.open("POST",link,true); //abre uma comunicação com o servidor através de uma requisição POST
-        //Se no servidor nao houver um elemento esperando por uma mensagem POST (ex. router.post()) para a rota /cadastro/update ocorrerar um erro: 404 - File Not Found
+        http.open("POST",link,true); 
 
-        //Dados HTML teria no cabecalho HEADER (da mensagem HTTP) - Content-Type= text/html
-        //Dados estruturados como querystring (ex: http//www.meu.com.br:3030/?campo=meu&campo2=10) -  Content-Type=x-www-form-urlencoded
-        //Dados no formato de Objeto Javascript para troca de informacoes (JSON) Content-Type=application/json : Ex.: {key1:value1,key2:value2}
-        http.setRequestHeader('Content-Type','application/json'); //constroi um cabecalho http para envio dos dados
+        
+        http.setRequestHeader('Content-Type','application/json');
          
-        for(let cont=0;cont<inputs.length;cont++){ //desabilita todos os inputs para escrita ou acesso (no caso do button)
+        for(let cont=0;cont<inputs.length;cont++){ 
             if(inputs[cont].disabled==true){
                 inputs[cont].disabled=false;
             } else inputs[cont].disabled=true;
         }
-    //    // essa suncao esta sendo colocada aqui só para dar uma parada e você poder ver os inputs desabilitados
-    //    //funcao que espera um tempo N, dado em milissegundos, e então chama uma função (callback). No caso, vamos usar 2000 ms = 2s
-    //    //essa funcao foi construida somente para que voce possa ver os inputs ficando desabilitados. Nao precisa usar.
-    //    function sleep(milliseconds) {
-    //         const date = Date.now();
-    //         let currentDate = null;
-    //         do {
-    //             currentDate = Date.now();
-    //         } while (currentDate - date < milliseconds);
-    //     }
-    //     console.log("Mostra essa mensagem no console, primeiro!");
-    //     sleep(2000)
-    //     console.log("Pronto, você consegue ver seus inputs desabilitados!");
-    //    //fim do codigo usado para ver os inputs desabiulitados
+    
 
-        //preenche um objeto com o indice da linha da tabela e os valores dos campos input do tipo text
-        data.id = index; //esse dado nao existe no vetor Users do lado do servidor (backend), mas preciso dele para apontar o indice do vetor que quero modificar
+       
+        data.id = index; 
         data.name = inputs[0].value;
         data.email = inputs[1].value;
         data.address = inputs[2].value;
@@ -82,28 +65,16 @@ function update(index,link){
         data.heigth = inputs[4].value;
         data.vote = inputs[5].value;
 
-        dataToSend = JSON.stringify(data); //transforma o objeto literal em uma string JSON que é a representação em string de um objeto JSON. Se quisesse o objeto no formato binario, usaria: JSON.parse(data)
+        dataToSend = JSON.stringify(data); 
 
-        http.send(dataToSend);//envia dados para o servidor na forma de JSO
+        http.send(dataToSend);
 
-        /* este codigo abaixo foi colocado para que a interface de cadastro so seja modificada quando se receber um aviso do servidor que a modificacao foi feita com sucesso. No caso o aviso vem na forma do codigo 200 de HTTP: OK */
+        
         http.onload = ()=>{ 
 
-            /*
-            readyState:
-            0: request not initialized
-            1: server connection established
-            2: request received
-            3: processing request
-            4: request finished and response is ready
-            status:
-            200: "OK"
-            403: "Forbidden"
-            404: "Page not found"
-            */
-            // baseado nos valores acima apresentados, o codigo abaixo mostra o que foi enviado pelo servidor como resposta ao envio de dados. No caso, se o request foi finalizado e o response foi recebido, a mensagem recebida do servidor eh mostrada no console do navegador. esse codigo foi feito apenas para verificar se tudo ocorreu bem no envio
+            
 
-            if (http.readyState === 4 && http.status === 200) { //testa se o envio foi bem sucedido
+            if (http.readyState === 4 && http.status === 200) { 
                 for(let cont=0;cont<spans.length;cont++){
                     if(spans[cont].className=="hidden"){
                         spans[cont].innerHTML = inputs[cont].value;
@@ -113,11 +84,11 @@ function update(index,link){
                     }
                 }
 
-                //esconde os campos de preenchimento para o cadastro
+                
                 for(let cont=0;cont<inputs.length;cont++){
                     if(inputs[cont].className=="show"){
                         inputs[cont].className="hidden";
-                        if(inputs[cont].disabled==false){//habilita novamente os inputs para escrita
+                        if(inputs[cont].disabled==false){
                             inputs[cont].disabled=true;
                         }
                     }
@@ -131,66 +102,32 @@ function update(index,link){
                 console.log("Ocorreu erro no processamento dos dados no servidor: ",http.responseText);
             }     
         }
-    /*
-    readyState:
-    0: request not initialized
-    1: server connection established
-    2: request received
-    3: processing request
-    4: request finished and response is ready
-    status:
-    200: "OK"
-    403: "Forbidden"
-    404: "Page not found"
-    */
-    // baseado nos valores acima apresentados, o codigo abaixo mostra o que foi enviado pelo servidor como resposta ao envio de dados. No caso, se o request foi finalizado e o response foi recebido, a mensagem recebida do servidor eh mostrada no console do navegador. esse codigo foi feito apenas para verificar se tudo ocorreu bem no envio
-
-    // http.onreadystatechange = (e)=>{
-    //     if (http.readyState === 4 && http.status === 200) { //testa se o envio foi bem sucedido
-    //         console.log(http.responseText);
-
-    //     }
-    // }
+   
 
     });  
 
 }
 
-function remove(index,_name,link){ //(index,link)
+function remove(index,_name,link){ 
 
-    //escuta se o botao foi clicado
+   
 
-    const http = new XMLHttpRequest(); //cria um objeto para requisição ao servidor
+    const http = new XMLHttpRequest(); 
     const url=link;
 
-    http.open("POST",link,true); //abre uma comunicação com o servidor através de uma requisição POST
-    http.setRequestHeader('Content-Type','application/json'); //constroi um cabecalho http para envio dos dados
+    http.open("POST",link,true); 
+    http.setRequestHeader('Content-Type','application/json'); 
 
-    //dataToSend = JSON.stringify({id:index}); //transforma o objeto literal em uma string JSON que é a representação em string de um objeto JSON
-    dataToSend = JSON.stringify({name:_name}); //transforma o objeto literal em uma string JSON que é a representação em string de um objeto JSON
+    
+    dataToSend = JSON.stringify({name:_name}); 
 
-    http.send(dataToSend);//envia dados para o servidor na forma de JSON
+    http.send(dataToSend);
 
-    /* este codigo abaixo foi colocado para que a interface de cadastro so seja modificada quando se receber um aviso do servidor que a modificacao foi feita com sucesso. No caso o aviso vem na forma do codigo 200 de HTTP: OK */
-
-    /*
-    readyState:
-    0: request not initialized
-    1: server connection established
-    2: request received
-    3: processing request
-    4: request finished and response is ready
-    status:
-    200: "OK"
-    403: "Forbidden"
-    404: "Page not found"
-    */
-
-    // baseado nos valores acima apresentados, o codigo abaixo mostra o que foi enviado pelo servidor como resposta ao envio de dados. No caso, se o request foi finalizado e o response foi recebido, a mensagem recebida do servidor eh mostrada no console do navegador. esse codigo foi feito apenas para verificar se tudo ocorreu bem no envio
+    
 
     http.onload = ()=>{ 
         
-        //seleciona todas as tags que sejam td 
+        
         let tr = document.querySelector(`table#list > tbody > tr[data-index-row='${index}']`);
 
         if (http.readyState === 4 && http.status === 200) {
